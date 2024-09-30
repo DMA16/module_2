44aea4f33f41bbfe11db026f215839364a4be67a
DMA16 <43100177+DMA16@users.noreply.github.com>
2024-09-30 21:50:51 +0300

    Add files via upload

---
+++ module_2_1.py
@@ -0,0 +1,10 @@
+first = 45
+second = 4
+third = 45
+
+if first == second and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
