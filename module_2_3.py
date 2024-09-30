cf55a8deeb5e6d1aa041ccfabc7478692b6b4247
DMA16 <43100177+DMA16@users.noreply.github.com>
2024-09-30 22:40:47 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,20 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+
+#Первый вариант кода
+i = 0
+while i < len(my_list) and my_list[i] >= 0:
+    if my_list[i] != 0:
+        print(my_list[i])
+    i += 1
+
+#Второй вариант кода
+i = 0
+while i < len(my_list):
+    if my_list[i] == 0:
+        continue
+
+    if my_list[i] < 0:
+        break
+
+    print(my_list[i])
